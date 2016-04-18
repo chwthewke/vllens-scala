1d3c7fa6accfd093c29c6ff1836baf4020ecb143
Chewie <chwthewke@gmail.com>
2016-04-18 09:51:50 +0200

    OK

--- src/main/scala/net/chwthewke/vllens/Fold.scala
+++ src/main/scala/net/chwthewke/vllens/Fold.scala
@@ -10,10 +10,13 @@ import classes.First
 abstract class Fold[S, A] {
   def runFold[F[_] : Contravariant : Applicative] : ( A => F[A] ) => ( S => F[S] )
 
+  // TODO foldRight
+
   def foldMap[R : Monoid]( f : A => R ) : S => R = { s =>
     runFold[Const[R, ?]].apply( ( a : A ) => Const( f( a ) ) ).apply( s ).getConst
   }
 
+  // TODO lazy
   def preview( s : S ) : Option[A] = foldMap[First[A]]( ( a : A ) => First( a.some ) ).apply( s ).getFirst
 }
 
@@ -21,19 +24,21 @@ object Fold {
   def folded[F[_] : Foldable, A] : Fold[F[A], A] =
     new Fold[F[A], A] {
 
-      override def runFold[G[_] : Contravariant : Applicative] : ( A => G[A] ) => ( F[A] => G[F[A]] ) = runFold1[G]
+      override def runFold[G[_] : Contravariant : Applicative] : ( A => G[A] ) => ( F[A] => G[F[A]] ) = runFold2[G]
 
       def runFold1[G[_] : Contravariant : Applicative] : ( A => G[A] ) => ( F[A] => G[F[A]] ) = {
         aga => fa => fa.traverse_[G, A]( aga ).map( _ => fa )
       }
 
-      def runFold2[G[_] : Contravariant](implicit A: Applicative[G]) : ( A => G[A] ) => F[A] => G[F[A]] = {
-        aga => fa =>
-          fa.foldRight(Eval.always(A.pure(fa))) { (a, lgfa) =>
-            lgfa.map(gfa => aga(a).map2(gfa)((_, b) => b))
-          }
+      def runFold2[G[_] : Contravariant]( implicit A : Applicative[G] ) : ( A => G[A] ) => F[A] => G[F[A]] = {
+        g =>
+          fa =>
 
-          ???
+            val noEff : G[A] = A.pure( () ).contramap( _ => () )
+
+            fa.foldRight[G[A]]( Eval.always( noEff ) )( ( a, lga ) =>
+              lga.map( ga => g( a ).map2( ga )( ( _, z ) => z ) ) )
+              .value.map( _ => () ).contramap( _ => () )
       }
     }
 }
--- src/test/scala/net/chwthewke/vllens/FoldSpec.scala
+++ src/test/scala/net/chwthewke/vllens/FoldSpec.scala
@@ -1,6 +1,8 @@
 package net.chwthewke.vllens
 
 import cats._
+import cats.data._
+import cats.std.list._
 import org.scalatest.{ Matchers, WordSpec }
 import scala.collection.{ mutable => scm }
 
@@ -13,7 +15,7 @@ class FoldSpec extends WordSpec with Matchers {
 
       val rec = RecFoldable( List( 1, 2, 3 ) )
 
-      Fold.folded[RecFoldable, Int].preview( rec ) should ===( Some( 1 ) )
+      Fold.folded[RecFoldable[List, ?], Int].preview( rec ) should ===( Some( 1 ) )
 
       rec.evals.toList should ===( List( 1 ) )
     }
@@ -22,17 +24,20 @@ class FoldSpec extends WordSpec with Matchers {
 
 object FoldSpec {
 
-  case class RecFoldable[A]( self : Traversable[A] ) {
+  case class RecFoldable[F[_], A]( self : F[A] ) {
     val evals : scm.Buffer[A] = new scm.ArrayBuffer[A]
   }
 
-  implicit val recFoldable : Foldable[RecFoldable] = new Foldable[RecFoldable] {
-    override def foldLeft[A, B]( fa : RecFoldable[A], b : B )( f : ( B, A ) => B ) : B = {
-      fa.self.foldLeft( b ) { ( b, a ) => fa.evals += a; f( b, a ) }
-    }
+  implicit def recFoldable[F[_]]( implicit F : Foldable[F] ) : Foldable[RecFoldable[F, ?]] =
+    new Foldable[RecFoldable[F, ?]] {
+      override def foldLeft[A, B]( fa : RecFoldable[F, A], b : B )( f : ( B, A ) => B ) : B = {
+        F.foldLeft( fa.self, b ) { ( b, a ) => fa.evals += a; f( b, a ) }
+      }
 
-    override def foldRight[A, B]( fa : RecFoldable[A], lb : Eval[B] )( f : ( A, Eval[B] ) => Eval[B] ) : Eval[B] = {
-      fa.self.foldRight( lb ) { ( a, lb ) => fa.evals += a; f( a, lb ) }
+      override def foldRight[A, B](
+        fa : RecFoldable[F, A], lb : Eval[B] )(
+          f : ( A, Eval[B] ) => Eval[B] ) : Eval[B] = {
+        F.foldRight( fa.self, lb ) { ( a, lb ) => fa.evals += a; f( a, lb ) }
+      }
     }
-  }
 }
